613de1e9b7c8baa6a0c1788dd367f516e184ac25
ariwisnu <wisnuari4658@gmail.com>
2020-11-17 20:05:43 +0800

    init project setup server menggunakan express install nodemon

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(() => {
+    console.log('hello server')
+     console.log('hello server')
+      console.log('hello server')
+
+})
+
+app.listen(4000);
+
